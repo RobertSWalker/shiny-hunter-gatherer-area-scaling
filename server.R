639afe35cf625ac8ccc1b31e83351f83b23574f0
Robert S Walker <44737920+RobertSWalker@users.noreply.github.com>
2018-11-22 09:45:56 -0600

    Add files via upload

---
+++ server.R
@@ -0,0 +1,31 @@
+# This is the server logic of a Shiny web application. You can run the application by clicking 'Run App' above.
+library(shiny)
+library(dplyr)
+library(cowplot)
+
+df <- read.csv("C:/users/walkerro/Desktop/R scripts/class/hgs.csv", header = TRUE)
+df <- df %>% dplyr::select("X","latitude","longitude","tlpop","area")
+model <- lm( log(area) ~ log(tlpop) + abs(latitude), data = df)
+
+# Define server logic
+server = function(input,output,session){
+  observeEvent( input$Enter, {
+    latitude = input$latitude
+    tlpop = input$tlpop
+    t = data.frame(latitude,tlpop)
+    t$area <- predict(model, t)
+    t$area <- round(exp(t$area),1)
+   
+    output$plot_foo = renderPlot({
+      ggplot(df, aes(x=tlpop, y=area)) + geom_point() + geom_smooth() + scale_x_continuous(trans="log10") + scale_y_continuous(trans="log10") +
+       geom_point(data = t, col = 'red', size = 5) + ylab("Home range area (sq km)") + xlab("Total population")
+        
+    })
+      
+   output$text<-renderText({
+        
+          return(paste("<span style=\"color:red; font-size: 20px\">Predicted home range area in square kilometers is </span>", t$area))
+          
+              })
+  })
+}
---
+++ ui.R
@@ -0,0 +1,30 @@
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+
+library(shiny)
+
+# Define UI for application
+shinyUI(fluidPage(
+  
+  # title
+  titlePanel("Use latitude and population to predict home range area of hunter-gatherers (Binford dataset)"),
+  
+  # Sidebar with input variables 
+  sidebarLayout(
+    sidebarPanel(
+       numericInput(inputId="latitude", label="Absolute latitude", 
+                    value=10,min=0, max=90),
+       numericInput(inputId="tlpop", label="Total population", 
+                    value=1000,min=0, max=10000000)
+       ),
+    
+    # get prediction
+    mainPanel(
+      actionButton("Enter", "Get prediction"),
+    #  htmlOutput(""),
+      htmlOutput("text"),
+      plotOutput("plot_foo")
+      
+    )
+  )
+))
